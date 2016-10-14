bdb2565c515a65fa17cec92d33227d5d4ce3a581
Mauricio Gofas <mauricio@gofas.net>
2016-10-14 03:25:12 -0300

    v1.1

--- auth.php
+++ auth.php
@@ -1,28 +1,25 @@
 <?php
 /**
- * Auto login auth for WHMCS
+ * Auto Login Hash Merge Tag Email Template
  * @author     Mauricio Gofas | gofas.net
- * @copyright  Copyright (c) 2016 Gofas.net
- *
- * Exemplo de URL para colar no template de email "Invoice Created" que leva o usuário para a visualização da fatura após o login:
- * {$whmcs_url}auth.php?email={$client_email}&hash={$hash}&whmcsurl={$whmcs_url}&goto=viewinvoice.php?id={$invoice_id}
- * 
- * Exemplo de URL para que leva o usuário para a visualização de um ticket respondido após o login:
- * {$whmcs_url}auth.php?email={$client_email}&hash={$hash}&whmcsurl={$whmcs_url}&goto={$ticket_link}
- *
- * Para endpoints personalizados altere o valor do parâmetro "goto", mais exemplos.: goto=&goto=viewinvoice.php?id={$invoice_id} | goto={$ticket_link} | goto={$whmcs_link}
+ * @copyright  Copyright (c) 2016 https://gofas.net
  */
+ 
+ // Modelo de URL para colar no template de email:
+// {$whmcs_url}auth.php?email={$client_email}&uid={$client_id}&uname={$client_first_name} {$client_last_name}&hash={$hash}&whmcsurl={$whmcs_url}&goto=viewinvoice.php?id={$invoice_id}
+
 $whmcsurl = $_GET["whmcsurl"].'dologin.php';
-$autoauthkey = "xxxxxxxxxxxxxxxxxxxxx_autoauthkey_xxxxxxxxxxxxxxxxxxxxx"; // chave igual à inserida no /configuration.php
-$secret_key =  "xxxxxxxxxxxxxxxxxxxxx_secret_key_xxxxxxxxxxxxxxxxxxxxx"; // chave igual à inserida no /includes/hooks/gofas_hash_email.php
-if (md5($secret_key) != $_GET["hash"]){
-die('Erro :(');
-}
+$autoauthkey = "xxxxxxxxxxxxxxxxxxxxx_autoauthkey_xxxxxxxxxxxxxxxxxxxxx"; // chave no /configuration.php
+$secret_key =  "xxxxxxxxxxxxxxxxxxxxx_secret_key_xxxxxxxxxxxxxxxxxxxxx"; // chave no /includes/hooks/gofas_hash_email.php
+
+if (md5($_GET['email'].$_GET['uid'].$_GET['uname'].$secret_key) != $_GET['hash']){
+die();
+} 
+$email	= $_GET['email'];
 $timestamp = time();
-$email = $_GET["email"];
-$goto = $_GET["goto"];
-$hash = sha1($email.$timestamp.$autoauthkey);
-$url = $whmcsurl."?email=$email&timestamp=$timestamp&hash=$hash&goto=".urlencode($goto);
+$goto      = $_GET["goto"];
+$hash      = sha1($email.$timestamp.$autoauthkey);
+$url       = $whmcsurl."?email=$email&timestamp=$timestamp&hash=$hash&goto=".urlencode($goto);
 header("Location: $url");
 exit;
 ?>
--- includes/hooks/gofas_hash_email.php
+++ includes/hooks/gofas_hash_email.php
@@ -1,13 +1,31 @@
 <?php
 /**
- * Auto Login Hash for Email Template
+ * Auto Login Hash Merge Tag Email Template
  * @author     Mauricio Gofas | gofas.net
- * @copyright  Copyright (c) 2016 Gofas.net
+ * @copyright  Copyright (c) 2016 https://gofas.net
  */
+
 if (!defined("WHMCS")) die("This file cannot be accessed directly");
 function gofas_hash_email_template($vars) {
-    $merge_fields = array();
-	$merge_fields['hash'] = md5('xxxxxxxxxxxxxxxxxxxxx_secret_key_xxxxxxxxxxxxxxxxxxxxx'); // chave igual à inserida no /auth.php
+	$whmcsAdmin					= 1;
+	$invoiceID					= $vars['relid'];
+	
+ 	$getinvoice					= 'getinvoice';
+	$getinvoiceid['invoiceid']	= $invoiceID;
+	$GetInvoiceResults			= localAPI($getinvoice,$getinvoiceid,$whmcsAdmin);
+	
+	$userID						= $GetInvoiceResults['userid'];
+	
+ 	$getClient							= "getclientsdetails";
+	$getClientValues["clientid"]		= $userID;
+	$getClientValues["stats"]			= true;
+	$getClientValues["responsetype"]	= "json";
+	$getClientResults					= localAPI($getClient,$getClientValues,$whmcsAdmin);
+	
+	$secret_key							= 'xxxxxxxxxxxxxxxxxxxxx_secret_key_xxxxxxxxxxxxxxxxxxxxx';
+	
+	$merge_fields = array();
+	$merge_fields['hash']	= md5($getClientResults['client']['email'].$getClientResults['client']['id'].$getClientResults['client']['fullname'].$secret_key);
 	return $merge_fields;
 }
 add_hook("EmailPreSend",1,"gofas_hash_email_template");
