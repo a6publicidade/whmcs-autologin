267593803c98bb8743b4aaa56dc96fa218d2dca2
Mauricio Gofas <mauricio@gofas.net>
2016-10-14 03:40:46 -0300

    v1.1

--- auth.php
+++ auth.php
@@ -8,10 +8,9 @@
  // Modelo de URL para colar no template de email:
 // {$whmcs_url}auth.php?email={$client_email}&uid={$client_id}&uname={$client_first_name} {$client_last_name}&hash={$hash}&whmcsurl={$whmcs_url}&goto=viewinvoice.php?id={$invoice_id}
 
-$whmcsurl = $_GET["whmcsurl"].'dologin.php';
 $autoauthkey = "xxxxxxxxxxxxxxxxxxxxx_autoauthkey_xxxxxxxxxxxxxxxxxxxxx"; // chave no /configuration.php
 $secret_key =  "xxxxxxxxxxxxxxxxxxxxx_secret_key_xxxxxxxxxxxxxxxxxxxxx"; // chave no /includes/hooks/gofas_hash_email.php
-
+$whmcsurl = $_GET["whmcsurl"].'dologin.php';
 if (md5($_GET['email'].$_GET['uid'].$_GET['uname'].$secret_key) != $_GET['hash']){
 die();
 } 
