a0f9e80928dbd371713a26b35ca0d8df631c3f4d
Mauricio Gofas <mauricio@gofas.net>
2016-10-14 03:32:25 -0300

    v1.1

--- includes/hooks/gofas_hash_email.php
+++ includes/hooks/gofas_hash_email.php
@@ -5,7 +5,7 @@
  * @copyright  Copyright (c) 2016 https://gofas.net
  */
 
-if (!defined("WHMCS")) die("This file cannot be accessed directly");
+if (!defined('WHMCS')) die('This file cannot be accessed directly');
 function gofas_hash_email_template($vars) {
 	$whmcsAdmin							= 1;
 	$invoiceID							= $vars['relid'];
@@ -13,15 +13,15 @@ function gofas_hash_email_template($vars) {
 	$getinvoiceid['invoiceid']			= $invoiceID;
 	$GetInvoiceResults					= localAPI($getinvoice,$getinvoiceid,$whmcsAdmin);
 	$userID								= $GetInvoiceResults['userid'];
- 	$getClient							= "getclientsdetails";
-	$getClientValues["clientid"]		= $userID;
-	$getClientValues["stats"]			= true;
-	$getClientValues["responsetype"]	= "json";
+ 	$getClient							= 'getclientsdetails';
+	$getClientValues['clientid']		= $userID;
+	$getClientValues['stats']			= true;
+	$getClientValues['responsetype']	= 'json';
 	$getClientResults					= localAPI($getClient,$getClientValues,$whmcsAdmin);
 	$secret_key							= 'xxxxxxxxxxxxxxxxxxxxx_secret_key_xxxxxxxxxxxxxxxxxxxxx';
 	$merge_fields = array();
 	$merge_fields['hash']				= md5($getClientResults['client']['email'].$getClientResults['client']['id'].$getClientResults['client']['fullname'].$secret_key);
 	return $merge_fields;
 }
-add_hook("EmailPreSend",1,"gofas_hash_email_template");
+add_hook('EmailPreSend',1,'gofas_hash_email_template');
 ?>
