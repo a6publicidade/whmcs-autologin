e76ca8373950c77ce401bb8c4ac23179587949f0
Mauricio Gofas <mauricio@gofas.net>
2016-12-05 13:40:39 -0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -6,7 +6,7 @@
  */
  
  // Modelo de URL para colar no template de email:
-// {$whmcs_url}auth.php?email={$client_email}&uid={$client_id}&uname={$client_first_name} {$client_last_name}&hash={$hash}&whmcsurl={$whmcs_url}&goto=viewinvoice.php?id={$invoice_id}
+// {$whmcs_url}auth.php?email={$client_email}&uid={$client_id}&uname={$client_first_name}{$client_last_name}&hash={$hash}&whmcsurl={$whmcs_url}&goto=viewinvoice.php?id={$invoice_id}
 
 $autoauthkey = "xxxxx_autoauthkey_xxxxx"; // chave igual à inserida no arquivo /configuration.php
 $secret_key =  "xxxxx_secret_key_xxxx"; // chave igual à inserida no arquivo /includes/hooks/gofas_hash_email.php
